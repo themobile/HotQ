9155fe50f6aa590f621e978cd3b0696fe4b2a7c5
danielchindea <chindea.daniel@gmail.com>
2014-01-16 15:33:30 +0200

    web admin

--- web-admin/js/filters.js
+++ web-admin/js/filters.js
@@ -2,9 +2,17 @@
 
 /* Filters */
 
-angular.module('hotq.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+angular.module('hotq.filters', [])
+    .filter('interpolate', ['version', function (version) {
+        return function (text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        }
+    }])
+
+    .filter('startFrom', function () {
+        return function (input, start) {
+            start = +start;
+            return input.slice(start);
+        }
+    })
+
